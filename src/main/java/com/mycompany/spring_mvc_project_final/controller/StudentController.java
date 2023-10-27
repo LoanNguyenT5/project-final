1c9f25b9bf8bab3507ef5f2d5a756898fb61007a
LoanNguyenThi5 <Loan.NguyenThi5@nashtechglobal.com>
2023-10-27 21:23:56 +0700

    update method send mail

--- src/main/java/com/mycompany/spring_mvc_project_final/controller/StudentController.java
+++ src/main/java/com/mycompany/spring_mvc_project_final/controller/StudentController.java
@@ -5,11 +5,15 @@ import com.mycompany.spring_mvc_project_final.repository.StudentRepository;
 import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
 import javax.servlet.http.HttpServletResponse;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -20,10 +24,30 @@ import java.util.List;
 @Controller
 @RequestMapping("/user")
 public class StudentController {
-
+	@Autowired
+	JavaMailSender javaMailSender;
 	@Autowired
 	StudentRepository studentRepository;
+	@RequestMapping(value = "/sendMail")
+	public void testSendMail() throws IOException {
+		// Send mail
+		System.out.println("Send Mail ==>");
+		sendEmail("loannguyen1501@gmail.com","Subject", "Hello all");
 
+	}
+	public void sendEmail(String recipient, String subject, String body) {
+		MimeMessage message = javaMailSender.createMimeMessage();
+		try {
+			MimeMessageHelper helper = new MimeMessageHelper(message, true);
+			helper.setTo(recipient);
+			helper.setSubject(subject);
+			helper.setText(body, true);
+			javaMailSender.send(message);
+		} catch (MessagingException e) {
+			e.printStackTrace();
+		}
+
+	}
 	@RequestMapping(value = "/insertImage", method = RequestMethod.POST,
 			consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
 			produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
